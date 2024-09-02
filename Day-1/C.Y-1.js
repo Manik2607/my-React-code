8933244ecab01e03c224f0b308c2970824e9dea6
= <=>
2024-09-02 09:23:20 +0530

    cy -1

---
+++ Day-1/C.Y-1.js
@@ -0,0 +1,17 @@
+import React from "react";
+function qn4(){
+  const namel="Kabil"
+  const name2="Kabil"
+  const object1={name:"Kabil"};
+  const object2={name:"Kabil"};
+  console.log(name1===name2);
+  console.log(object1===object2);
+}
+const Greeting = () => {
+    return(
+        <div>
+            <button onClick={qn4}>Click</button>
+        </div>
+    )
+}
+export default Greeting;
