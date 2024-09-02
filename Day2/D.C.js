0259330a8e84d78e4be329f1a1c91bf99ad1c7f2
= <=>
2024-09-02 09:27:59 +0530

    dc

---
+++ Day2/D.C.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const D2DC = () => {
+  return(
+    <div style={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center',backgroundColor:'lightskyblue'}}>
+      <h1 >Car and Location Info</h1>
+      <p style={{textAlign:'center',backgroundColor:'lightgray',padding:'100px',fontFamily:'monospace',fontSize:'30px'}} >Porsche Model <br></br>
+      <img src="https://imgd-ct.aeplcdn.com/1056x660/n/xsoupsa_1476828.jpg?q=80" alt="Porsche" style={{ width:'500px',height:'300px'}}></img>
+      </p>
+    </div>
+  );
+};
+
+export default D2DC;
